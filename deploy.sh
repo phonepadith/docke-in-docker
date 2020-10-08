54c3b47f2553fcf19dc6c6178b0c04db2ab79a09
unknown <phonepadithpp@gmail.com>
2020-10-08 17:10:41 +0700

    update all sh file 2

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 ssh root@206.189.41.85<<EOF
-   cd /var/www/html/
-   git pull origin master
-   npm install
-   pm2 restart all
-   exit
+ cd ~/webdeploy
+ git pull
+ npm install — production
+ pm2 restart all
+ exit
 EOF
